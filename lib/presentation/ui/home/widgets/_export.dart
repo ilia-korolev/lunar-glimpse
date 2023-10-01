5bf8eb5982eb2d9112dd336e610f0e10c307fc2f
Ilia Korolev <ikorolev.dev@gmail.com>
2023-10-01 13:13:06 +0900

    remove unused code

--- lib/presentation/ui/home/widgets/_export.dart
+++ lib/presentation/ui/home/widgets/_export.dart
@@ -2,7 +2,6 @@ export 'animated_settings_icon.dart';
 export 'home_app_bar.dart';
 export 'home_navigation_bar.dart';
 export 'image_content.dart';
-export 'leaderboard_button.dart';
 export 'loading_media_card.dart';
 export 'media_card.dart';
 export 'page_switcher.dart';
