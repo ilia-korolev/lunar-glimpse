ced94f9ec50ec10615a7c4af083fc5d303e518c0
Ilia Korolev <ikorolev.dev@gmail.com>
2023-10-15 14:07:01 +0900

    remove unused assets

--- lib/app/global/asset_names.dart
+++ lib/app/global/asset_names.dart
@@ -9,9 +9,5 @@ class _Animations {
 
   static const _animationsPath = '$_basePath/animations';
 
-  String get spaceCoffee => '$_animationsPath/space_coffee.riv';
-  String get spaceCoffeeAlt => '$_animationsPath/space_coffee_alt.riv';
-  String get octopusLoop => '$_animationsPath/octopus_loop.riv';
-  String get astronautInMug => '$_animationsPath/astronaut_in_mug.json';
   String get astronautOnPlanet => '$_animationsPath/astronaut_on_planet.json';
 }
