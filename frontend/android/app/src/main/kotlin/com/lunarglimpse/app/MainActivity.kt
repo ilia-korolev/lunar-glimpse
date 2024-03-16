f4bc5da191798f74e227d32362a15ecc8c1542fd
Ilia Korolev <ikorolev.dev@gmail.com>
2024-03-16 22:12:49 +0900

    [frontend] add a splash screen to android

--- frontend/android/app/src/main/kotlin/com/lunarglimpse/app/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/lunarglimpse/app/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.lunarglimpse.app
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+    }
+}
